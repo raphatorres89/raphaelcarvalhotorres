302fcaaed73f0893bb30d1d0c253f51973c8feb4
Raphael Torres <rapha.oo.torres@gmail.com>
2017-12-08 11:19:10 -0200

    Atualização de Educação

--- js/custom.js
+++ js/custom.js
@@ -2,3 +2,41 @@
   $(document).ready(function(){
     $('.collapsible').collapsible();
   });
+  
+  // gráfico de mapa
+  var options = {
+	legend: {
+	  display: false
+	},
+	responsive: true,
+	scale: {
+	  ticks: {
+		display: false,
+		beginAtZero: true,
+		max: 10
+	  }
+	}
+  };
+
+  var dados = {
+	labels: ["HTML5", "CSS", "JavaScript", "Java EE", "MySQL"],
+	datasets: [
+	  {
+		label: "Minhas habilidades",
+		backgroundColor: "rgba(151,187,205,0.2)",
+		borderColor: "rgba(151,187,205,1)",
+		pointBackgroundColor: "rgba(151,187,205,1)",
+		pointBorderColor: "#fff",
+		pointHighlightFill: "#fff",
+		pointHighlightStroke: "rgba(151,187,205,1)",
+		data: [8, 7, 3, 8, 6]
+	  }
+	]
+  };
+
+  var ctx = document.getElementById('minhasHabilidades').getContext('2d');
+  var myRadarChart = new Chart(ctx, {
+	type: 'radar',
+	data: dados,
+	options: options
+  });
