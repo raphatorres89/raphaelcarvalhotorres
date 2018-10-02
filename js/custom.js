77c5438128aec1782432ae028105432d92741113
Raphael Torres <rapha.oo.torres@gmail.com>
2018-10-02 12:00:39 -0300

    Habilidades atualizadas
    Adicionado Laravel e Spring

--- js/custom.js
+++ js/custom.js
@@ -19,7 +19,7 @@
   };
 
   var dados = {
-	labels: ["HTML5", "CSS", "JavaScript", "Java EE", "Java SE", "MySQL"],
+	labels: ["HTML5", "CSS", "JavaScript", "Java EE", "Java SE", "Spring", "MySQL", "Laravel"],
 	datasets: [
 	  {
 		label: "Minhas habilidades",
@@ -29,7 +29,7 @@
 		pointBorderColor: "#fff",
 		pointHighlightFill: "#fff",
 		pointHighlightStroke: "rgba(151,187,205,1)",
-		data: [8, 7, 3, 8, 3, 6]
+		data: [8, 7, 3, 8, 8, 5, 6, 1]
 	  }
 	]
   };
