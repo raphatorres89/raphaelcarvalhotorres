7d5d08179c00c70209d39607706db611746bf701
Raphael Torres <rapha.oo.torres@gmail.com>
2018-12-10 14:16:48 -0200

    Add Git

--- js/custom.js
+++ js/custom.js
@@ -19,7 +19,7 @@
   };
 
   var dados = {
-	labels: ["HTML5", "CSS", "JavaScript", "Java EE", "Java SE", "Spring", "MySQL", "Laravel"],
+	labels: ["HTML5", "CSS", "JavaScript", "Java EE", "Java SE", "Spring", "MySQL", "Laravel", "GIT"],
 	datasets: [
 	  {
 		label: "Minhas habilidades",
@@ -29,7 +29,7 @@
 		pointBorderColor: "#fff",
 		pointHighlightFill: "#fff",
 		pointHighlightStroke: "rgba(151,187,205,1)",
-		data: [8, 7, 3, 8, 8, 5, 6, 1]
+		data: [8, 7, 3, 8, 7, 5, 6, 1, 4]
 	  }
 	]
   };
